7f7fcbdd78e75135a327eae263b7b22be6fd386f
Erick <ericksjp703@gmail.com>
2024-05-05 19:15:13 -0300

    finish initial setup

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <div className="home">
-      <h1>Home</h1>
-      <Button>clike me</Button>
-    </div>
-  );
+  return <div>Home</div>;
 }
